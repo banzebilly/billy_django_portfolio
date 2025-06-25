9afb9a1716ee6437c086babe351438853c9cd7b3
banzebilly <bbanze49gmail.com>
2025-06-25 19:11:37 +0200

    added db.sqlite3

--- billyCV_project/settings.py
+++ billyCV_project/settings.py
@@ -25,9 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+from decouple import config
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'banzebillydev.co.za']
+
 
 
 # Application definition
