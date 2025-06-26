59ef69245c13474d23d54f653ff74f50fe8a13c0
banzebilly <bbanze49gmail.com>
2025-06-26 20:58:20 +0200

    modified the build.sh file

--- build.sh
+++ build.sh
@@ -2,6 +2,18 @@
 
 #!/usr/bin/env bash
 # Exit on error
+# set -o errexit
+
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+
+# if [[ $CREATE_SUPERUSER ]]; then
+#    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+# fi
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
 pip install -r requirements.txt
@@ -9,6 +21,18 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]; then
-   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+if [[ $CREATE_SUPERUSER == "True" ]]; then
+  echo "Checking if superuser exists..."
+  python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = "${DJANGO_SUPERUSER_USERNAME}"
+email = "${DJANGO_SUPERUSER_EMAIL}"
+password = "${DJANGO_SUPERUSER_PASSWORD}"
+if not User.objects.filter(username=username).exists():
+    print("Creating superuser...")
+    User.objects.create_superuser(username=username, email=email, password=password)
+else:
+    print("Superuser already exists, skipping creation.")
+EOF
 fi
